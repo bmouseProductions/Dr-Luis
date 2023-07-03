816404a4794bd5aed0333a173b24fe26b088cbbe
João Victor Corado <jhonisilva545@gmail.com>
2023-07-03 16:19:09 -0300

    pos-operatorio

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Router } from "../router";
+import { Router } from "./router";
 import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
